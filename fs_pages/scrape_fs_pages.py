727df6a5f63814f2d0b78ebf41db72c55d2359da
JamesM813 <jrm803@gmail.com>
2024-07-18 12:11:07 -0400

    Fixed indentation and data extraction errors, now updating to work with empty profiles

--- fs_pages/scrape_fs_pages.py
+++ fs_pages/scrape_fs_pages.py
@@ -1,11 +1,9 @@
 import logging, csv, os, time, sys 
-root_dir = os.path.abspath(dynamic_location(__file__,  '../')) 
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../')) 
 sys.path.insert(0, root_dir) 
 from selenium import webdriver 
 from selenium.webdriver.common.by import By 
-from selenium.webdriver.chrome.options import Options 
-from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.chrome.options import Options
 from utils import start_time, end_time, read_from_csv, reset_log, announce_progress, dynamic_location
 
 logging.basicConfig( 
@@ -13,40 +11,51 @@ logging.basicConfig(
     level=logging.DEBUG, 
     format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S' 
     ) 
+def flatten(s): 
+    flat = ''
+    if s:
+        for line in s.splitlines():
+            flat = flat + line
+        return flat
+    else:
+        return 'N/A'
 
-def process_url(driver, url, staff_data): 
-    try: 
-        driver.get(url) 
-        while True: 
-            try: 
-                names = driver.find_elements(By.CLASS_NAME,'fsConstituentProfileLink') 
-                titles = driver.find_elements(By.CLASS_NAME,'fsTitles') 
-                emails = driver.find_elements(By.CLASS_NAME,'fsEmail') 
-                time.sleep(0.5) 
-                min_length = min(len(names), len(titles), len(emails)) 
-                for i in range(min_length):
-                    # All of these values are formatted very strangely, however its safer to just store the values and then
-                    # normalize them later, as to not format any of them incorrectly.
-                    try: staff_data.append(
-                        { 'name': names[i].text, 
-                         'title': titles[i].text,
-                         'email': emails[i].text 
-                         }) 
-                    except Exception as e: logging.error(f'Error: {e}') 
-                    next_button = WebDriverWait(driver, 3).until( EC.element_to_be_clickable((By.CLASS_NAME, "fsNextPageLink")) ) 
-                    if next_button.is_displayed(): 
-                        next_button.click() 
-                        time.sleep(0.5) 
-                    else: break 
-            except Exception as e: logging.error(f'Error clicking next button at {url}') 
-            break 
-    except Exception as e: 
-        logging.error(f'Could not get {url}: {e}') 
+def process_url(driver, url, staff_data):
+    try:
+        driver.get(url)
+        while True:
+            names = driver.find_elements(By.CLASS_NAME, 'fsConstituentProfileLink')
+            titles = driver.find_elements(By.CLASS_NAME, 'fsTitles')
+            emails = driver.find_elements(By.CLASS_NAME, 'fsEmail')
+
+            min_length = min(len(names), len(titles), len(emails))
+            for i in range(min_length):
+                try:
+                    # For now, the data is not formatted (besides being flattened for csv purposes) as it varies a good amount.
+                    # It will be normalized with another script after all the data has been stored. 
+                    staff_member = {
+                        'name': flatten(names[i].text),
+                        'title': flatten(titles[i].text),
+                        'email': flatten(emails[i].text)
+                    }
+                    staff_data.append(staff_member)
+                except Exception as e:
+                    logging.error(f'Error processing data: {e}')
+
+            next_button = driver.find_element(By.CLASS_NAME, "fsNextPageLink")
+            if next_button.is_displayed():
+                next_button.click()
+                time.sleep(2)
+            else:
+                break
+
+    except Exception as e:
+        logging.error(f'Error processing URL {url}: {e}')
     
 def main(): 
     start = start_time() 
-    reset_log(dynamic_location(__file__, 'scraping.log')) 
-    staff_directories = read_from_csv(dynamic_location(__file__,'fs_page_urls.csv'))
+    reset_log(dynamic_location(__file__, 'scraping.log'))
+    staff_directories = read_from_csv(dynamic_location(__file__, 'fs_page_urls.csv'))
     staff_data = [] 
     chrome_options = Options() 
     #chrome_options.add_argument("--headless") 
